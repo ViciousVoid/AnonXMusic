5e8583906ab249b65056c0733dbe0fa3a1892b7f
ViciousVoid <viciousvoidd@gmail.com>
2024-06-21 10:33:31 +0530

    Update bot.py
    Signed-off-by: ViciousVoid <viciousvoidd@gmail.com>

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -10,10 +10,10 @@ class Anony(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="AnonXMusic",
+            name="vicivo",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
+            bot_token=config.7283326973:AAGU5vqzvx5rq3ThyR1TzKjSPkbWK-o8x9c,
             in_memory=True,
             parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
